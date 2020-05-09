423e3f064520d7e3182c69455ed09a8514adf36c
Michael Hancock <michael.hancock@protonmail.com>
2020-05-09 13:47:26 +0100

    Add MatrixBuilder utility

---
+++ src/RayTracer/Model/MatrixBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RayTracer.Model
+{
+    public class MatrixBuilder
+    {
+        public MatrixBuilder()
+            : this(0)
+        {
+        }
+
+        public MatrixBuilder(int rowCapacity)
+        {
+            _data = new List<float[]>(rowCapacity);
+        }
+
+        public MatrixBuilder WithRow(params float[] values)
+        {
+            if (_expectedWidth.HasValue && values.Length != _expectedWidth)
+            {
+                throw new ArgumentException("Rows must all have the same number of values");
+            }
+            else if (!_expectedWidth.HasValue)
+            {
+                _expectedWidth = values.Length;
+            }
+
+            _data.Add(values);
+
+            return this;
+        }
+
+
+        public Matrix Create()
+        {
+            if (!_expectedWidth.HasValue)
+            {
+                return new Matrix(0, 0);
+            }
+
+            var matrix = new Matrix(_data.Count, _expectedWidth.Value);
+
+            for (int i = 0; i < _data.Count; i++)
+            {
+                for (int j = 0; j < _expectedWidth.Value; j++)
+                {
+                    matrix.Set(_data[i][j], i, j);
+                }
+            }
+
+            return matrix;
+        }
+
+        private readonly List<float[]> _data;
+        private int? _expectedWidth;
+    }
+}
---
+++ test/RayTracer.UnitTests/ModelTests/MatrixBuilderTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using RayTracer.Model;
+using Xunit;
+
+namespace RayTracer.UnitTests.ModelTests
+{
+    public class MatrixBuilderTests
+    {
+        [Fact]
+        public void MatrixBuilder_CanConstructEmptyMatrix()
+        {
+            // Arrange
+            var builder = new MatrixBuilder();
+
+            // Act
+            var matrix = builder
+                .Create();
+
+            // Assert
+            Assert.Equal(0, matrix.Rows);
+            Assert.Equal(0, matrix.Columns);
+        }
+
+        [Fact]
+        public void MatrixBuilder_CanConstructMatrix()
+        {
+            // Arrange
+            var builder = new MatrixBuilder();
+
+            // Act
+            var matrix = builder
+                .WithRow(1, 2, 3)
+                .WithRow(4, 5, 6)
+                .WithRow(7, 8, 9)
+                .WithRow(10, 11, 12)
+                .Create();
+
+            // Assert
+            Assert.Equal(4, matrix.Rows);
+            Assert.Equal(3, matrix.Columns);
+        }
+
+        [Fact]
+        public void MatrixBuilder_WithRow_ThrowsIfInconsistentColumnsAreUsed()
+        {
+            // Arrange
+            var builder = new MatrixBuilder();
+
+            // Act
+            builder
+                .WithRow(1, 2, 3)
+                .WithRow(4, 5, 6)
+                .WithRow(7, 8, 9);
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => builder.WithRow(10, 11));
+            Assert.Throws<ArgumentException>(() => builder.WithRow(10, 11, 12, 13));
+        }
+    }
+}
